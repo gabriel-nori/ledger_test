c2b82557249edfcf38f1013de92a1e6e6d018222
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-01-24 16:44:01 -0300

    Feat: Refactor tests for financial institution

--- backend/apps/financial_institution/test_helpers.py
+++ backend/apps/financial_institution/test_helpers.py
@@ -1,52 +1,58 @@
-from apps.location import test_helpers as locationHelpers
+from apps.location.test_helpers import TestObjects as LocationObjects
 from apps.financial_institution.models import *
 
-institution_type = InstitutionType(name="bank")
+class TestObjects():
 
-alpha_bank = Institution(
-    name="Alpha Bank",
-    type=institution_type,
-    code="12409",
-    country=locationHelpers.brazil
-)
+    def create(self):
+        self. location_objects = LocationObjects()
+        self.location_objects.create()
 
-not_a_bank = Institution(
-    name="Not a Bank",
-    type=institution_type,
-    code="09832",
-    country=locationHelpers.brazil
-)
+        self.institution_type = InstitutionType.objects.get_or_create(name="bank")[0]
 
-good_savings_bank = Institution(
-    name="Good Savings Bank",
-    type=institution_type,
-    code="95430",
-    country=locationHelpers.argentina
-)
+        self.alpha_bank = Institution.objects.get_or_create(
+            name="Alpha Bank",
+            type=self.institution_type,
+            code="12409",
+            country=self.location_objects.brazil
+        )[0]
 
-alpha_bank_01 = Branch(
-    institution=alpha_bank,
-    street=locationHelpers.croata,
-    number=123,
-    floor=None,
-    room=None,
-    code=None
-)
+        self.not_a_bank = Institution.objects.get_or_create(
+            name="Not a Bank",
+            type=self.institution_type,
+            code="09832",
+            country=self.location_objects.brazil
+        )[0]
 
-not_a_bank_01 = Branch(
-    institution=not_a_bank,
-    street=locationHelpers.croata,
-    number=201,
-    floor=None,
-    room=None,
-    code=None
-)
+        self.good_savings_bank = Institution.objects.get_or_create(
+            name="Good Savings Bank",
+            type=self.institution_type,
+            code="95430",
+            country=self.location_objects.argentina
+        )[0]
 
-good_savings_bank_01 = Branch(
-    institution=good_savings_bank,
-    street=locationHelpers.acassuso,
-    number=91,
-    floor=None,
-    room=None,
-    code=None
-)
+        self.alpha_bank_01 = Branch.objects.get_or_create(
+            institution=self.alpha_bank,
+            street=self.location_objects.croata,
+            number=123,
+            floor=None,
+            room=None,
+            code="saar4"
+        )[0]
+
+        self.not_a_bank_01 = Branch.objects.get_or_create(
+            institution=self.not_a_bank,
+            street=self.location_objects.croata,
+            number=201,
+            floor=None,
+            room=None,
+            code="ssfg"
+        )[0]
+
+        self.good_savings_bank_01 = Branch.objects.get_or_create(
+            institution=self.good_savings_bank,
+            street=self.location_objects.acassuso,
+            number=91,
+            floor=None,
+            room=None,
+            code="xxfa"
+        )[0]
--- backend/apps/financial_institution/tests.py
+++ backend/apps/financial_institution/tests.py
@@ -1,55 +1,13 @@
-from apps.financial_institution.models import Branch, BranchClient, Institution, InstitutionType
-from apps.location.models import Country, Street
+from apps.financial_institution.test_helpers import TestObjects
 from django.test import TestCase
 
 class TestFinancialInstitution(TestCase):
     def setUp(self):
-        institution_type = InstitutionType.objects.create(name="bank")
-
-        alpha_bank = Institution.objects.create(
-            name="Alpha Bank",
-            type=institution_type,
-            code="12409",
-            country=Country.objects.get(name="Brazil")
-        )
-
-        not_a_bank = Institution.objects.create(
-            name="Not a Bank",
-            type=institution_type,
-            code="09832",
-            country=Country.objects.get(name="Brazil")
-        )
-
-        good_savings_bank = Institution.objects.create(
-            name="Good Savings Bank",
-            type=institution_type,
-            code="95430",
-            country=Country.objects.get(name="Argentina")
-        )
-
-        alpha_bank_01 = Branch.objects.create(
-            institution=alpha_bank,
-            street=Street.objects.get(name="Croata"),
-            number=123,
-            floor=None,
-            room=None,
-            code=None
-        )
-
-        not_a_bank_01 = Branch.objects.create(
-            institution=not_a_bank,
-            street=Street.objects.get(name="Croata"),
-            number=201,
-            floor=None,
-            room=None,
-            code=None
-        )
-
-        good_savings_bank_01 = Branch.objects.create(
-            institution=good_savings_bank,
-            street=Street.objects.get(name="Acasusso"),
-            number=91,
-            floor=None,
-            room=None,
-            code=None
-        )
+        self.test_objects = TestObjects()
+        self.test_objects.create()
+    
+    def test_branch_consistency(self):
+        branch = self.test_objects.alpha_bank_01
+        assert branch.code == "saar4"
+        assert branch.institution.code == "12409"
+        
