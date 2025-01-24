026d2fd02e379d24e342380e6d16f12cd74450b5
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-01-24 16:54:41 -0300

    Feat: Implement testing for person using external object

--- backend/apps/person/test_helpers.py
+++ backend/apps/person/test_helpers.py
@@ -1,44 +1,58 @@
 from apps.person.models import Person, Occupation
+from django.contrib.auth import get_user_model
 from datetime import date
 
-child_occupation = Occupation(name="child")
-occupation = Occupation(name="tester")
-underage_client = Person(
-    name="john doe",
-    birthday=date(2021, 12, 21),
-    sex="F",
-    gender="F",
-    primary_email="teste@teste.com",
-    occupation=child_occupation,
-    document="ajbsjbajsbas"
-)
+class TestObjects():
+    child_occupation = None
+    occupation = None
+    underage_client = None
+    approved_client = None
+    fernando_ok = None
+    maria_ok = None
 
-approved_client = Person(
-    name="Michael Kyle",
-    birthday=date(1960, 9, 4),
-    sex="M",
-    gender="M",
-    primary_email="teste@teste.com",
-    occupation=occupation,
-    document="ajbsjbajsbas"
-)
+    def create(self):
+        self.child_occupation = Occupation.objects.get_or_create(name="child")[0]
+        self.occupation = Occupation.objects.get_or_create(name="tester")[0]
+        self.underage_client = Person.objects.get_or_create(
+            name="john doe",
+            birthday=date(2021, 12, 21),
+            sex="F",
+            gender="F",
+            primary_email="teste@teste.com",
+            occupation=self.child_occupation,
+            document="ajbsjbajsbas",
+            user=get_user_model().objects.get_or_create(username="test_user_1")[0]
+        )[0]
 
-fernando_ok = Person(
-    name="Fernando",
-    birthday=date(1990, 12, 5),
-    sex="M",
-    gender="M",
-    primary_email="teste@teste.com",
-    occupation=occupation,
-    document="ajbsjbajsbas"
-)
+        self.michael_ok = Person.objects.get_or_create(
+            name="Michael Kyle",
+            birthday=date(1960, 9, 4),
+            sex="M",
+            gender="M",
+            primary_email="teste@teste.com",
+            occupation=self.occupation,
+            document="ajbsjbajsbas",
+            user=get_user_model().objects.get_or_create(username="test_user_2")[0]
+        )[0]
 
-maria_ok = Person(
-    name="Michael Kyle",
-    birthday=date(2000, 1, 27),
-    sex="F",
-    gender="F",
-    primary_email="teste@teste.com",
-    occupation=occupation,
-    document="ajbsjbajsbas"
-)
+        self.fernando_ok = Person.objects.get_or_create(
+            name="Fernando",
+            birthday=date(1990, 12, 5),
+            sex="M",
+            gender="M",
+            primary_email="teste@teste.com",
+            occupation=self.occupation,
+            document="ajbsjbajsbas",
+            user=get_user_model().objects.get_or_create(username="test_user_3")[0]
+        )[0]
+
+        self.maria_ok = Person.objects.get_or_create(
+            name="Michael Kyle",
+            birthday=date(2000, 1, 27),
+            sex="F",
+            gender="F",
+            primary_email="teste@teste.com",
+            occupation=self.occupation,
+            document="ajbsjbajsbas",
+            user=get_user_model().objects.get_or_create(username="test_user_4")[0]
+        )[0]
--- backend/apps/person/tests.py
+++ backend/apps/person/tests.py
@@ -1,75 +1,12 @@
-from apps.financial_institution.models import Branch
-from apps.person.models import Person, Occupation
+from apps.person.test_helpers import TestObjects
 from apps.person import utils as personUtils
-from django.contrib.auth import get_user_model
-from apps.account.models import Account
 from datetime import date, timedelta
 from django.test import TestCase
 
 class PersonTest(TestCase):
     def setUp(self):
-        self.kyle_account = Account(
-            account_holder=Person.objects.get_or_create(
-                primary_email="michael@teste.com",
-                defaults={
-                    "name":"Michael Kyle",
-                    "birthday":date(1960, 9, 4),
-                    "sex":"M",
-                    "gender":"M",
-                    "primary_email":"michael@teste.com",
-                    "occupation":Occupation.objects.get_or_create(name="developer")[0],
-                    "document":"michael",
-                    "user":get_user_model().objects.get_or_create(username="test0")[0]
-                }
-            )[0],
-            institution_branch=Branch.objects.get(code="09832"),
-            identifier="12121212",
-            overdraft_protection=True,
-            overdraft_limit=250000,
-            balance=0
-        )
-
-        self.fernando_account = Account(
-            account_holder=Person.objects.get_or_create(
-                primary_email="michael@teste.com",
-                defaults={
-                    "name":"Fernando",
-                    "birthday":date(1960, 9, 4),
-                    "sex":"M",
-                    "gender":"M",
-                    "primary_email":"michael@teste.com",
-                    "occupation":Occupation.objects.get_or_create(name="developer")[0],
-                    "document":"michael",
-                    "user":get_user_model().objects.get_or_create(username="test2")[0]
-                }
-            )[0],
-            institution_branch=Branch.objects.get(code="12409"),
-            identifier="12121212",
-            overdraft_protection=True,
-            overdraft_limit=250000,
-            balance=0
-        )
-
-        self.maria_account = Account(
-            account_holder=Person.objects.get_or_create(
-                primary_email="michael@teste.com",
-                defaults={
-                    "name":"Maria",
-                    "birthday":date(1960, 9, 4),
-                    "sex":"M",
-                    "gender":"M",
-                    "primary_email":"michael@teste.com",
-                    "occupation":Occupation.objects.get_or_create(name="developer")[0],
-                    "document":"michael",
-                    "user":get_user_model().objects.get_or_create(username="test")[0]
-                }
-            )[0],
-            institution_branch=Branch.objects.get(code="95430"),
-            identifier="12121212",
-            overdraft_protection=False,
-            overdraft_limit=250000,
-            balance=0
-        )
+        self.test_objects = TestObjects()
+        self.test_objects.create()
 
     def test_age(self):
         """
@@ -77,12 +14,14 @@ class PersonTest(TestCase):
         In this case, our client can't be younger than 16 years.
         """
 
-        assert self.maria_account.birthday <= date.today()-timedelta(days=16*365)
-        assert self.kyle_account.birthday <= date.today()-timedelta(days=16*365)
+        assert self.test_objects.underage_client.birthday >= date.today()-timedelta(days=16*365)
+        assert self.test_objects.michael_ok.birthday <= date.today()-timedelta(days=16*365)
 
     def test_age_validators(self):
         """
         Let's check if the validators are working correctly to check if the client is older than 16 and/or older than 18
         """
-        assert personUtils.check_16_older(self.kyle_account.birthday)
-        assert personUtils.check_18_older(self.maria_account.birthday)
+        assert not personUtils.check_16_older(self.test_objects.underage_client.birthday)
+        assert not personUtils.check_18_older(self.test_objects.underage_client.birthday)
+        assert personUtils.check_16_older(self.test_objects.michael_ok.birthday)
+        assert personUtils.check_18_older(self.test_objects.michael_ok.birthday)
