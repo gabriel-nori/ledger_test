5d3f9210c6ff7407ca4a8080235674764253c51f
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-01-24 11:46:56 -0300

    Feat: include openapi specification endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework.schemas import get_schema_view
 from apps.account.views import AccountListView
 from apps.person.views import SigninView
 from django.urls import include, path
@@ -24,6 +25,15 @@ router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        "api/specs/",
+        get_schema_view(
+            title="Ledger API specification",
+            description="API specification",
+            version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
     path('api/', include('rest_framework.urls')),
     path('api/signin/', SigninView.as_view())
 ]
